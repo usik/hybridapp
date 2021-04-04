27f7f829a3aa84f128261fa509c127a1e120f175
Chaesoo Rim <chaesoo.rim@navercorp.com>
2021-04-04 18:11:22 +0900

    learning express

---
+++ hybridapp/app.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const admin = require('./routes/admin');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res) => {
+    res.send('express start');
+});
+
+//Routing
+app.use('/admin', admin);
+
+app.listen( port, () => {
+    console.log('Express listening on port', port);
+});
