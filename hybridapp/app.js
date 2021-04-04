78538243a40fa4466c643dcdb1432571ee0d8889
yusik Kim <usik.o2@gmail.com>
2021-04-05 08:45:39 +0900

    router/controller/express setting

--- hybridapp/app.js
+++ hybridapp/app.js
@@ -1,40 +1,91 @@
-
 const express = require('express');
 const nunjucks = require('nunjucks');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const admin = require('./routes/admin');
-
-const app = express();
-const port = 3000;
-
-nunjucks.configure('template', {
-    autoescape: true,
-    express: app
-});
-
-//middleware settings
-app.use(logger('dev'));
-app.use(express.json()); //app.use(bodyParser.json()) deprecated
-app.use(express.urlencoded({extended:false}));
 
 
-//routing path 추가
-app.use('/uploads', express.static('uploads'));
+class App {
 
-//login variable
-app.use( (req, res,next) => {
-    app.locals.isLogin = false;
-    next();
-});
+    constructor () {
+        this.app = express();
+        
+        // 뷰엔진 셋팅
+        this.setViewEngine();
 
-app.get('/', (req,res) => {
-    res.send('express start');
-});
+        // 미들웨어 셋팅
+        this.setMiddleWare();
 
-//Routing
-app.use('/admin', admin);
+        // 정적 디렉토리 추가
+        this.setStatic();
 
-app.listen( port, () => {
-    console.log('Express listening on port', port);
-});
+        // 로컬 변수
+        this.setLocals();
+
+        // 라우팅
+        this.getRouting();
+
+        // 404 페이지를 찾을수가 없음
+        this.status404();
+
+        // 에러처리
+        this.errorHandler();
+
+
+    }
+
+
+    setMiddleWare (){
+        
+        // 미들웨어 셋팅
+        this.app.use(logger('dev'));
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+
+    }
+
+    setViewEngine (){
+
+        nunjucks.configure('template', {
+            autoescape: true,
+            express: this.app
+        });
+
+    }
+
+
+    setStatic (){
+        this.app.use('/uploads', express.static('uploads'));
+    }
+
+    setLocals(){
+
+        // 템플릿 변수
+        this.app.use( (req, res, next) => {
+            this.app.locals.isLogin = true;
+            this.app.locals.req_path = req.path;
+            next();
+        });
+
+    }
+
+    getRouting (){
+        this.app.use(require('./controllers'))
+    }
+
+    status404() {        
+        this.app.use( ( req , res, _ ) => {
+            res.status(404).render('common/404.html')
+        });
+    }
+
+    errorHandler() {
+
+        this.app.use( (err, req, res,  _ ) => {
+            res.status(500).render('common/500.html')
+        });
+    
+    }
+
+}
+
+module.exports = new App().app;
--- hybridapp/routes/admin.js
+++ hybridapp/routes/admin.js
@@ -1,28 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-function testMiddleWare(req, res, next){
+function testMiddleWare( req, res, next ){
     console.log('첫번째 미들웨어');
     next();
-};
+}
 
-function testMiddleWare2(req, res, next){
+function testMiddleWare2( req, res, next ){
     console.log('두번째 미들웨어');
     next();
-};
+}
 
-router.get('/', testMiddleWare, testMiddleWare2, (req,res)=>{
+router.get('/', testMiddleWare, testMiddleWare2 , (req,res) => {
     res.send('admin app');
-
-
 });
 
-router.get('/products', function(_, res){
+router.get('/products', ( _ , res) => {
     res.render( 'admin/products.html' , 
-        {message: "hello"}
+        { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
     );
-
-
 });
 
 router.get('/products/write', ( _ , res) => {
