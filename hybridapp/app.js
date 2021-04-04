4381720a9eb6de4c9d4f6d0a11856ee847671324
Chaesoo Rim <chaesoo.rim@navercorp.com>
2021-04-04 18:40:34 +0900

    nunjucks templating

--- hybridapp/app.js
+++ hybridapp/app.js
@@ -1,10 +1,22 @@
 
 const express = require('express');
+const nunjucks = require('nunjucks');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 const admin = require('./routes/admin');
 
 const app = express();
 const port = 3000;
 
+nunjucks.configure('template', {
+    autoescape: true,
+    express: app
+});
+
+//middleware settings
+app.use(logger('dev'));
+app.use(express.json()); //app.use(bodyParser.json()) deprecated
+app.use(express.urlencoded({extended:false}));
 app.get('/', (req,res) => {
     res.send('express start');
 });
